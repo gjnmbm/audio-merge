736a45f8b56fe655be2e8faf9fe53fab57a5d676
gjnmbm <gjnmbm@gmail.com>
2021-03-05 12:26:21 -0800

    Edit merge function

--- audioMerge.cpp
+++ audioMerge.cpp
@@ -1,6 +1,28 @@
 #include <fstream>
 #include <iostream>
 
+using namespace std;
+
 int main() {
+    fstream audioFile;
+    fstream outFile; //For now, I will see if I can make a copy of an audio file, then I will see if
+    //I can append audio data from one audio file to another
+    streampos size;
+    char* audioData;
+    audioFile.open("audio bits/a.wav", ios::binary | ios::ate);
+    outFile.open("test.wav", ios::binary);
+    if (audioFile.is_open()) {
+        size = audioFile.tellg();
+        audioData = new char[size];
+        audioFile.seekg(0, ios::beg);
+        audioFile.read(audioData, size);
+        outFile.write(audioData, size);
+        delete[] audioData;
+    }
+    else {
+        cout << "Unable to open file";
+    }
+    audioFile.close();
+    outFile.close();    
     return 0;
 }
