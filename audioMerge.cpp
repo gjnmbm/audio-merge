e5c2ab770a37651cf99eaf6e56ca59013052655e
repl.it user <replituser@example.com>
2021-03-18 12:19:38 +0000

    Change to match test

--- audioMerge.cpp
+++ audioMerge.cpp
@@ -1,28 +1,36 @@
-#include <fstream>
 #include <iostream>
-
+#include <fstream>
+//A space for learning about the WAV format
 using namespace std;
 
 int main() {
-    fstream audioFile;
-    fstream outFile; //For now, I will see if I can make a copy of an audio file, then I will see if
-    //I can append audio data from one audio file to another
-    streampos size;
-    char* audioData;
-    audioFile.open("audio bits/a.wav", ios::binary | ios::ate);
-    outFile.open("test.wav", ios::binary);
-    if (audioFile.is_open()) {
-        size = audioFile.tellg();
-        audioData = new char[size];
-        audioFile.seekg(0, ios::beg);
-        audioFile.read(audioData, size);
-        outFile.write(audioData, size);
-        delete[] audioData;
-    }
-    else {
-        cout << "Unable to open file";
-    }
-    audioFile.close();
-    outFile.close();    
-    return 0;
+  ifstream wavFile("audioBits/a.wav", ios::binary);
+  ofstream dataFile("data.wav", ios::binary);
+  int chunkSize = 4;
+  //int soundDataSize = 4; //Subchunk2Size
+  //int riffSize = 12; //Riff size 
+  //int fmtSize = 4; //Subchunk1Size
+  char* soundData;
+  //Do I need these other two things in order to make a copy of a WAV file?
+  //char* riff;
+  //char* fmt; 
+  uint16_t intData = 0; //In order to capture size of chunks in int form 
+  if (wavFile.is_open()) {
+    wavFile.seekg(4);
+    wavFile.read((char*)(&intData), chunkSize); //Regular casting seems to work
+    cout << intData << '\n';
+    chunkSize = intData + 8;
+    wavFile.seekg(0);
+    soundData = new char[chunkSize];
+    wavFile.read(soundData, chunkSize);
+    dataFile.write(soundData, chunkSize);
+    cout << chunkSize << "\n";
+    delete[] soundData; 
+  }
+  if (wavFile.is_open()) {
+    wavFile.close();
+  }
+  dataFile.close();
+  return 0;
 }
+  
